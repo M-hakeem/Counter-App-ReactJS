e12a008e13d86c618516ac4fee93206171a70682
M-hakeem <mohammedabdulhakeem5@gmail.com>
2021-10-28 02:13:11 +0100

    counter app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css";
+// import ListItem  from './Components/ListItem';
+// import Car from'./exercises/Car';
+ import CountingNo from './Assignments/Count';
+
+
+
+ReactDOM.render(<CountingNo/>,document.getElementById('root'));
+
